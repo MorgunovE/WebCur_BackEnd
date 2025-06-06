37f5b3c3c1a64a0c6484984534b90fce187ba73e
Morgunov Evgenii <58105584+MorgunovE@users.noreply.github.com>
2025-06-06 18:46:48 -0400

    add JWT logout resource implementation

--- resources/auth_resource.py
+++ resources/auth_resource.py
@@ -22,11 +22,11 @@ class AuthentificationRessource(Resource):
             return {"access_token": access_token}, 200
         return {"message": "Identifiants invalides"}, 401
 
-    # Ressource pour la déconnexion (JWT)
-    class DeconnexionRessource(Resource):
-        def post(self):
-            # Implementation de la déconnexion doit faire partie de la gestion du token JWT dnas Frontend party
-            # En général, la déconnexion est gérée côté client en supprimant le token
-            # Ici, nous pouvons juste retourner un message de succès comme exemple
-            # Frontend peut supprimer le token JWT du stockage local ou des cookies
-            return {"message": "Déconnexion réussie"}, 200
+# Ressource pour la déconnexion (JWT)
+class DeconnexionRessource(Resource):
+    def post(self):
+        # Implementation de la déconnexion doit faire partie de la gestion du token JWT dnas Frontend party
+        # En général, la déconnexion est gérée côté client en supprimant le token
+        # Ici, nous pouvons juste retourner un message de succès comme exemple
+        # Frontend peut supprimer le token JWT du stockage local ou des cookies
+        return {"message": "Déconnexion réussie"}, 200
