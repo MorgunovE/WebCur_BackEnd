79a2e1fae30e77da71e63c1ce2bce64f2cb90ced
Morgunov Evgenii <58105584+MorgunovE@users.noreply.github.com>
2025-06-06 18:15:39 -0400

    refactor user schema to simplify field descriptions

--- schemas/user.py
+++ schemas/user.py
@@ -2,6 +2,6 @@ from marshmallow import Schema, fields
 
 class UtilisateurSchema(Schema):
     id = fields.Str(dump_only=True)
-    email = fields.Email(required=True, description="Adresse e-mail de l'utilisateur")
-    mot_de_passe = fields.Str(required=True, load_only=True, description="Mot de passe")
-    nom_utilisateur = fields.Str(required=True, description="Nom d'utilisateur")
+    email = fields.Email(required=True)
+    mot_de_passe = fields.Str(required=True, load_only=True)
+    nom_utilisateur = fields.Str(required=True)
