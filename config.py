59ccc9b8e5421295546c6810845d9744da2e9acb
Morgunov Evgenii <58105584+MorgunovE@users.noreply.github.com>
2025-06-06 17:37:18 -0400

    add MONGODB_DBNAME configuration to support database name in config

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     MONGODB_URI = os.getenv("MONGODB_URI")
+    MONGODB_DBNAME = os.getenv("MONGODB_DBNAME")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET")
     SWAGGER_URL = "/swagger"
     API_URL = "/static/swagger.json"
