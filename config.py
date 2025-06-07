4f8267b5acec34580043516fa02c75af90209329
Morgunov Evgenii <58105584+MorgunovE@users.noreply.github.com>
2025-06-06 20:06:19 -0400

    feat: add JWT access token expiration configuration to the app

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+from datetime import timedelta
+
 
 class Config:
     MONGODB_URI = os.getenv("MONGODB_URI")
@@ -6,3 +8,6 @@ class Config:
     JWT_SECRET_KEY = os.getenv("JWT_SECRET")
     SWAGGER_URL = "/swagger"
     API_URL = "/static/swagger.json"
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(
+        minutes=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES"))
+    )
